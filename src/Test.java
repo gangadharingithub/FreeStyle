84bdbb98337a354d8436ee59c18ed2c77463fa48
mamillapalli gangadhar <mailme.m.gangadhar@gmail.com>
2016-04-21 06:35:13 +0530

    adding files

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import org.joda.time.DateTime;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DateTime dateTime = new DateTime();
+		System.out.println("Date Time :" + dateTime);
+
+	}
+
+}
